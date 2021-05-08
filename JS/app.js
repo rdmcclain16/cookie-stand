7a4c8d9e4f02087648dabcd84a888a625650e91f
rdmcclain16 <74519618+rdmcclain16@users.noreply.github.com>
2021-05-08 10:23:48 -0500

    added more code to test console

--- JS/app.js
+++ JS/app.js
@@ -11,86 +11,131 @@ const seattle = {
     guestEachHour:[],
     cookiesEachHour: [],
     totalDailyCookies: 0,
+    calcGuestsPerHr: function() {
+        for(let i = 0; i < hours.length; i++) {
+        this.guestEachHr.push(random(this.minGuestPerHr, this.maxGuestPerHr))
+        }
+    },
 
-    randomCookies: function() {
-        console.log(Math.floor(Math.random() * 65) + 23);
-    } 
-} 
-  seattle.randomCookies()
+    calcCookiesPerHr: function() {
+        this.calcGuestsPerHr();
+        for(let i = 0; i < hours.length; i++) {
+            const oneHour = Math.ceil(this.guestEachHour[i] * this.avgCookiePerGuest)
+            this.cookiesEachHour.push(oneHour);
+            this.totalDailyCookies += oneHour;
+        }
+    },
+   render() {
+       this.calcCookiesPerHr();
+       const unorderedList = document.getElementById('seattle')
+       for(let i = 0; i < hours.length; i++) {
+       const listItem = document.createElement('li')
+       listItem.textContent = hours[i] + ': ' + this.cookiesEachHour[i] + ' cookies '
+        unorderedList.appendChild(listItem);
+   
+         console.log(listItem)
+       }
 
-const tokyo = {
-    city: 'Tokyo',
-    minGuestPerHr: 3,
-    maxGuestPerHr: 24,
-    avgCookiePerGuest: 1.2,
-    guestEachHour:[],
-    cookiesEachHour: [],
-    totalDailyCookies: 0,
+       const listItem = document.createElement('li');
+       listItem.textContent = "Total: " + this.totalDailyCookies + ' cookies';
+       unorderedList.appendChild(listItem);
+    }
+};
 
-    randomCookies: function() {
-        console.log(Math.floor(Math.random() * 24) + 3);
-    } 
-} 
-    tokyo.randomCookies()
+    function random(min, max) {
+        return Math.floor(math.random() * (max - min + 1)) + min;
+    }
+
+    const allStores = [
+        seattle
+    ];
+
+    function renderAllStores() {
+        for(let i = 0; i < allStores.length; i++) {
+            allStores[i].render();
+        }
+    })()
+ 
+
+//   seattle.randomCookies()
+
+
+
+// const tokyo = {
+//     city: 'Tokyo',
+//     minGuestPerHr: 3,
+//     maxGuestPerHr: 24,
+//     avgCookiePerGuest: 1.2,
+//     guestEachHour:[],
+//     cookiesEachHour: [],
+//     totalDailyCookies: 0,
+
+//     randomCookies: function() {
+//         console.log(Math.floor(Math.random() * 24) + 3);
+//     } 
+// } 
+//     tokyo.randomCookies()
+
+
     
-//     render(); {
+// //     render(); {
 
-//      }    
+// //      }    
 
-const dubai = {
-    city: 'Dubai',
-    minGuestPerHr: 11,
-    maxGuestPerHr: 38,
-    avgCookiePerGuest: 3.7,
-    guestEachHour:[],
-    cookiesEachHour: [],
-    totalDailyCookies: 0,
+// const dubai = {
+//     city: 'Dubai',
+//     minGuestPerHr: 11,
+//     maxGuestPerHr: 38,
+//     avgCookiePerGuest: 3.7,
+//     guestEachHour:[],
+//     cookiesEachHour: [],
+//     totalDailyCookies: 0,
 
-    randomCookies: function() {
-        console.log(Math.floor(Math.random() * 38) + 11);
-    }
-} 
-    dubai.randomCookies()
+//     randomCookies: function() {
+//         console.log(Math.floor(Math.random() * 38) + 11);
+//     }
+// } 
+//     dubai.randomCookies()
 
-// render(); {
+// // render(); {
 
-// }
+// // }
 
 
 
-const paris = {
-    city: 'Paris',
-    minGuestPerHr: 20,
-    maxGuestPerHr: 38,
-    avgCookiePerGuest: 2.3,
-    guestEachHour:[],
-    cookiesEachHour: [],
-    totalDailyCookies: 0,
+// const paris = {
+//     city: 'Paris',
+//     minGuestPerHr: 20,
+//     maxGuestPerHr: 38,
+//     avgCookiePerGuest: 2.3,
+//     guestEachHour:[],
+//     cookiesEachHour: [],
+//     totalDailyCookies: 0,
     
-    randomCookies: function() {
-        console.log(Math.floor(Math.random() * 38) + 23);
-    }
-} 
-    paris.randomCookies()
+//     randomCookies: function() {
+//         console.log(Math.floor(Math.random() * 38) + 23);
+//     }
+// } 
+//     paris.randomCookies()
 
-//     render(); {
+// //     render(); {
 
-//      }    
+// //      }    
 
-const lima = {
-    city: 'Lima',
-    minGuestPerHr: 2,
-    maxGuestPerHr: 16,
-    avgCookiePerGuest: 4.6,
-    guestEachHour:[],
-    cookiesEachHour: [],
-    totalDailyCookies: 0,
-    randomCookies: function() {
-        console.log(Math.floor(Math.random() * 16) + 2);
-    }
-} 
-    lima.randomCookies()
+// const lima = {
+//     city: 'Lima',
+//     minGuestPerHr: 2,
+//     maxGuestPerHr: 16,
+//     avgCookiePerGuest: 4.6,
+//     guestEachHour:[],
+//     cookiesEachHour: [],
+//     totalDailyCookies: 0,
+//     randomCookies: function() {
+//         console.log(Math.floor(Math.random() * 16) + 2);
+//     }
+// } 
+//     lima.randomCookies()
 
-//     render(); {
+// //     render(); {
 
 //      }
